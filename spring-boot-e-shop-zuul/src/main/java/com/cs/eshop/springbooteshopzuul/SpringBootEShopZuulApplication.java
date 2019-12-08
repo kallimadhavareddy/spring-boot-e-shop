b66a33511254fb2e7cbe219eef9b34f4c264213b
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-12-08 23:59:51 +0100

    e-shop mvp BasketCatalog service implementation and Sevice Discovery server and client for product,customer,basket and Figen changes

--- spring-boot-e-shop-zuul/src/main/java/com/cs/eshop/springbooteshopzuul/SpringBootEShopZuulApplication.java
+++ spring-boot-e-shop-zuul/src/main/java/com/cs/eshop/springbooteshopzuul/SpringBootEShopZuulApplication.java
@@ -2,8 +2,12 @@ package com.cs.eshop.springbooteshopzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class SpringBootEShopZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEShopZuulApplication.class, args);
