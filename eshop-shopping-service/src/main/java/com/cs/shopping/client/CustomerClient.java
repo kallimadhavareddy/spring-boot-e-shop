75693ab062ac8d0ca8476a6d5ae6a0d0e45788c1
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-12-13 01:55:07 +0100

    fiegn not working

--- eshop-shopping-service/src/main/java/com/cs/shopping/client/CustomerClient.java
+++ eshop-shopping-service/src/main/java/com/cs/shopping/client/CustomerClient.java
@@ -1,19 +1,24 @@
 package com.cs.shopping.client;
 
 import com.cs.shopping.data.Customer;
+import com.cs.shopping.util.ObjectMapperUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@FeignClient(name = "eshop-customer-client")
+//@FeignClient(name = "eshop-customer-client")
+
+@FeignClient(name="eshop-customer-client",url="http://localhost:8888/customer/api/service1")
 @Service
 public interface CustomerClient {
     @GetMapping("/customers/{customerId}")
     Customer findCustomerById(@PathVariable("customerId") int customerId);
     @GetMapping("/customer/")
-    Customer getCustomer(@RequestParam("emailId") final String emailId);
+    List getCustomer(@RequestParam("emailId") final String emailId);
 }
--- eshop-shopping-service/src/main/java/com/cs/shopping/resource/FeignResource.java
+++ eshop-shopping-service/src/main/java/com/cs/shopping/resource/FeignResource.java
@@ -2,20 +2,28 @@ package com.cs.shopping.resource;
 
 import com.cs.shopping.client.CustomerClient;
 import com.cs.shopping.data.Customer;
+import com.cs.shopping.util.ObjectMapperUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 public class FeignResource {
     @Autowired
     CustomerClient customerClient;
+    @Autowired
+    ObjectMapperUtils objectMapperUtils;
     @GetMapping("/feign/test/")
-    public Customer getCustomer(@RequestParam("emailId") final String emailId){
+    public List<Customer> getCustomer(@RequestParam("emailId") final String emailId){
         log.info("Testing feign client ");
-        return customerClient.getCustomer(emailId);
+        List customers = customerClient.getCustomer(emailId);
+        log.info("customers{}",customers.size());
+        return  objectMapperUtils.mapAll(customers,Customer.class);
     }
 }
